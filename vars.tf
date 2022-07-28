0ae20bf5e8d7be80ceb66cd78666be2fe4cc50c9
Owi Wei How <owiweihow@gmail.com>
2022-07-28 23:20:01 +0800

    try

--- vars.tf
+++ vars.tf
@@ -1,16 +1,12 @@
 terraform {
   required_providers {
-    # local = {
-    #   source  = "hashicorp/local"
-    #   version = ">= 2.1.0"
-    # }
-    # null = {
-    #   source  = "hashicorp/null"
-    #   version = ">= 3.1.0"
-    # }
-    oci = {
-      source  = "hashicorp/oci"
-      version = ">= 3.30.0"
+    local = {
+      source  = "hashicorp/local"
+      version = ">= 2.1.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 3.1.0"
     }
   }
   required_version = ">= 1.0"
