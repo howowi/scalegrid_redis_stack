9d38746ed6fddb8cafe0342ed64896ec83882a99
Owi Wei How <owiweihow@gmail.com>
2022-07-28 19:28:24 +0800

    try

--- deploy_redis.tf
+++ deploy_redis.tf
@@ -9,7 +9,12 @@ resource "null_resource" "scalegrid_deploy_cluster" {
     depends_on = [null_resource.scalegrid_login]
     
     provisioner "local-exec" {
-        command = "curl -i https://console.scalegrid.io/RedisClusters/create -b cookiejar.txt -d '{'clusterName': '${var.cluster_name}', 'version': '${var.redis_version}', 'serverCount': ${var.server_count}, 'shardCount': ${var.shard_count}, 'size': '${var.size}', 'backupIntervalInHours': ${var.backup_interval}, 'machinePoolIDList': ['${var.machine_pool_list}'], 'cidrList': ['"10.1.0.0/24"'], 'encryptDisk': true }'"
+        command = "touch input.json && echo '{'clusterName': '${var.cluster_name}', 'version': '${var.redis_version}', 'serverCount': ${var.server_count}, 'shardCount': ${var.shard_count}, 'size': '${var.size}', 'backupIntervalInHours': ${var.backup_interval}, 'machinePoolIDList': ['${var.machine_pool_list}'], 'cidrList': ['${var.cidr_list}'], 'encryptDisk': true }' > input.json"
+        interpreter = [ "/bin/bash","-c"]
+    }
+
+    provisioner "local-exec" {
+        command = "curl -i https://console.scalegrid.io/RedisClusters/create -b cookiejar.txt -d input.json"
         interpreter = [ "/bin/bash","-c"]
     }
 }
