0a435f72d43c7a3c77adb5c1d5657d640d69ce9b
Owi Wei How <owiweihow@gmail.com>
2022-07-28 22:09:26 +0800

    test

--- deploy_redis.tf
+++ deploy_redis.tf
@@ -7,14 +7,14 @@ resource "null_resource" "scalegrid_login" {
 
 resource "null_resource" "scalegrid_deploy_cluster" {
     depends_on = [null_resource.scalegrid_login]
-    
-    provisioner "local-exec" {
-        command = "touch input.json && echo '{'clusterName': '${var.cluster_name}', 'version': '${var.redis_version}', 'serverCount': ${var.server_count}, 'shardCount': ${var.shard_count}, 'size': '${var.size}', 'backupIntervalInHours': ${var.backup_interval}, 'machinePoolIDList': ['${var.machine_pool_list}'], 'cidrList': ['${var.cidr_list}'], 'encryptDisk': true }' > input.json"
-        interpreter = [ "/bin/bash","-c"]
-    }
 
     provisioner "local-exec" {
-        command = "curl -i https://console.scalegrid.io/RedisClusters/create -b cookiejar.txt -d input.json"
+        command = <<EOT
+            curl -k https://console.scalegrid.io/RedisClusters/create \
+            -b cookiejar.txt \
+            -H 'Content-Type: application/json' \
+            --data-raw '{"clusterName": "${var.cluster_name}", "version": "${var.redis_version}", "serverCount": ${var.server_count}, "shardCount": ${var.shard_count}, "size": "${var.size}", "backupIntervalInHours": ${var.backup_interval}, "machinePoolIDList": [${var.machine_pool_list}], "cidrList": ["${var.cidr_list}"], "encryptDisk": true }'
+        EOT
         interpreter = [ "/bin/bash","-c"]
     }
 }
