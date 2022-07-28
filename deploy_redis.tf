4d6b63e97fa42203f8a483ff6e1ae7f9e9f5a78e
Owi Wei How <owiweihow@gmail.com>
2022-07-28 18:52:19 +0800

    fixed

--- deploy_redis.tf
+++ deploy_redis.tf
@@ -8,7 +8,7 @@ resource "null_resource" "scalegrid_login" {
 resource "null_resource" "scalegrid_deploy_cluster" {
     depends_on = [null_resource.scalegrid_login]
     provisioner "local-exec" {
-        command = "curl -i https://console.scalegrid.io/RedisClusters/create -b cookiejar.txt -d '{'clusterName': '${var.cluster_name}', 'version': '${var.redis_version}', 'serverCount': ${var.server_count}, 'shardCount': ${var.shard_count}, 'size': '${var.size}', 'backupIntervalInHours': ${var.backup_interval}, 'machinePoolIDList': ['${var.machine_pool_list}']}, 'cidrList': ['${var.cidr_list}']'"
+        command = "curl -i https://console.scalegrid.io/RedisClusters/create -b cookiejar.txt -d '{'clusterName': '${var.cluster_name}', 'version': '${var.redis_version}', 'serverCount': ${var.server_count}, 'shardCount': ${var.shard_count}, 'size': '${var.size}', 'backupIntervalInHours': ${var.backup_interval}, 'machinePoolIDList': ['${var.machine_pool_list}'], 'cidrList': ['${var.cidr_list}']}'"
         interpreter = [ "/bin/bash","-c"]
     }
 }
