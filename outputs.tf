f946037946d46d0c836d7359a70737673eedccb5
Owi Wei How <owiweihow@gmail.com>
2022-08-30 15:26:59 +0800

    added monitoring

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,10 @@ output scalegrid_redis_url {
   value = "https://console.scalegrid.io/RedisClusters/${data.local_file.cluster_id.content}/clusterDetails"
 }
 
+output redis_monitoring {
+  value = "https://monitoring.scalegrid.io/Telemetryapplication/${data.local_file.cluster_id.content}/monitoring"
+}
+
 output cluster_name {
   value = var.cluster_name
 }
