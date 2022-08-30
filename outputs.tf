7ae68b2d706c858cd026cb7765fb360a6d0f8caa
Owi Wei How <owiweihow@gmail.com>
2022-08-30 11:32:35 +0800

    added output

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,15 @@
 output scalegrid_redis_url {
   value = "https://console.scalegrid.io/application/redisclusters"
 }
+
+output cluster_name {
+  value = var.cluster_name
+}
+
+output size {
+  value = var.size
+}
+
+output machine_pool_list {
+  value = var.machine_pool_list
+}
