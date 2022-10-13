af33aebb4ac791b8a915d57a5e670de0edcd6cd6
howowi <owiweihow@gmail.com>
2022-10-13 18:50:51 +0800

    updated

--- deploy_redis.tf
+++ deploy_redis.tf
@@ -13,7 +13,7 @@ resource "null_resource" "scalegrid_deploy_cluster" {
             CLUSTER_ID=$(curl -k https://console.scalegrid.io/RedisClusters/create \
             -b cookiejar.txt \
             -H 'Content-Type: application/json' \
-            --data-raw '{"clusterName": "${var.cluster_name}", "version": "${var.redis_version}", "serverCount": ${var.server_count}, "shardCount": ${var.shard_count}, "size": "${var.size}", "backupIntervalInHours": ${var.backup_interval}, "machinePoolIDList": [${var.machine_pool_map[var.machine_pool_list]}], "cidrList": ["${var.cidr_list}"], "encryptDisk": true }' \
+            --data-raw '{"clusterName": "${var.cluster_name}", "version": "${var.redis_version_map[var.redis_version]}", "serverCount": ${var.server_count}, "shardCount": ${var.shard_count}, "size": "${var.size_map[var.size]}", "backupIntervalInHours": ${var.backup_interval}, "machinePoolIDList": [${var.machine_pool_map[var.machine_pool_list]}], "cidrList": ["${var.cidr_list}"], "encryptDisk": true }' \
             | jq -r '.clusterID') && echo -n $CLUSTER_ID > cluster_id.txt
         EOT
         interpreter = [ "/bin/bash","-c"]
--- vars.tf
+++ vars.tf
@@ -18,6 +18,15 @@ variable "cluster_name" {
 variable "redis_version" {
 }
 
+variable "redis_version_map" {
+  type = map
+  default = {
+    "v6.2.5" = "V625"
+    "v6.0.4" = "V604"
+    "v5.0.5" = "V505"
+  }
+}
+
 variable "server_count" {
 }
 
@@ -25,6 +34,17 @@ variable "shard_count" {
 }
 
 variable "size"{
+}
+
+variable "size_map"{
+  type = map
+  default = {
+    "Micro(2 vCPU,2GB RAM, 1/8 baseline)" = "Micro"
+    "Small(4 vCPU,4GB RAM, 1/2 baseline)" = "Small"
+    "Medium(2 vCPU,8GB RAM)" = "Medium"
+    "Large(4 vCPU,16GB RAM)" = "Large"
+    "Xlarge(4 vCPU,30GB RAM)" = "Xlarge"
+  }
 }
 
 variable "cidr_list"{
